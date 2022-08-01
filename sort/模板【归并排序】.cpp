ab06986664b39c2eae28846f17e55c21d94a5225
naruto <2577484662@qq.com>
2022-08-01 16:12:45 +0800

    8.1

---
+++ sort/模板【归并排序】.cpp
@@ -0,0 +1,33 @@
+//先进行加法，后进行右移 
+//先递归，后合并 
+//i从l开始到r结束，j从0开始 
+#include<iostream>
+using namespace std;
+
+const int N=100010;
+int n;
+int q[N],tmp[N];
+
+void merge_sort(int l,int r)
+{
+	if(l==r) return;
+	int mid=l+r>>1; //先进行加法，后进行右移 
+	merge_sort(l,mid); merge_sort(mid+1,r); //先递归，后合并 
+	int k=0,i=l,j=mid+1;
+	while(i<=mid&&j<=r)
+	{
+		if(q[i]<=q[j]) tmp[k++]=q[i++];
+		else tmp[k++]=q[j++];
+	}
+	while(i<=mid) tmp[k++]=q[i++];
+	while(j<=r) tmp[k++]=q[j++];
+	for(int i=l,j=0;i<=r;i++,j++) q[i]=tmp[j]; //i从l开始到r结束，j从0开始 
+}
+int main()
+{
+	cin>>n;
+	for(int i=0;i<n;i++) cin>>q[i];
+	merge_sort(0,n-1);
+	for(int i=0;i<n;i++) cout<<q[i]<<" ";
+	return 0;
+ } 
